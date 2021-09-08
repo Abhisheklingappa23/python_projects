7abae218b99906027ccf9292189bb9b42653bc19
Abhishek L <84961641+Abhisheklingappa23@users.noreply.github.com>
2021-09-08 10:48:15 +0530

    Delete admission directory

--- admission/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from admission.models import Student
-#from django.forms import fields, models
-#from admission.models import Student
-
-class StudentModelForm(forms.ModelForm):
-    class Meta:
-        model=Student
-        fields = '__all__'
