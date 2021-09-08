ac9b1fecd19391dc49e1c4c69c5aea2435d03355
Abhishek L <84961641+Abhisheklingappa23@users.noreply.github.com>
2021-09-08 10:44:44 +0530

    Delete schoolapp/admission directory

--- schoolapp/admission/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from admission.models import Student
-# Register your models here.
-#admin.site.register(Student) ///or this can also used
-class StudentAdmin(admin.ModelAdmin):
-    list_display=['id','name','father','class_name','contact']
-
-admin.site.register(Student,StudentAdmin)
